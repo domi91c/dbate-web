3b402aa8871d8e560fa0109173ec4ec7f7ea361d
Dominic Nunes <dominic.n@me.com>
2022-03-21 18:38:21 -0400

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <BaseNavbar />
+  <div class="px-2 sm:px-6 lg:px-8 text-gray-200">
+    <div class="container">
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <NuxtLayout name="default">
+    <h1>hello world</h1>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+</script>
